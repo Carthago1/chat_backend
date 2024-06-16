0610fc31a62bd71c0beac561815950d4b988fa3d
Verin Dmitriy <dima.verin.2002@mail.ru>
2024-06-16 15:32:20 +0300

    check CI with error in server.ts

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(cors({
 }));
 
 app.use('/api/auth', authRoutes);
-app.use('/api', userRoutes, chatRoutes);
+app.use('/api', userRoutes, chatRoutes)
 
 const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
